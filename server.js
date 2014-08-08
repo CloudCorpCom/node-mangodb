7cae65851f6481dfb203cf2a821000ec45a04681
Baz O'Mahony <bomahony@shutterstock.com>
2014-08-08 08:48:07 -0600

    MAking it match MangoDB API

--- server.js
+++ server.js
@@ -10,15 +10,28 @@ console.log("MangoDB server listening on "+config.port);
 
 server.on("connection",function(sock) {
     var byteCount = 0;
-    console.log("Connection: "+sock.remoteAddress+":"+sock.remotePort);
+    var clientInfo = sock.remoteAddress+":"+sock.remotePort;
+    console.log("Connection: "+clientInfo);
+    sock.write('HELLO\r\n');
     
     sock.on("data",function(data) {
-        byteCount += data.length;
-        devnull.write(data);
+        var cmd_bits = data.toString('utf8').trim().split(' ');
+        var cmd = cmd_bits[0];
+        if(cmd==="BYE") {
+            return sock.end("BYE "+byteCount+"\r\n");
+        }
+        if(cmd==="WAIT") {
+            console.log("Waiting?");
+        }
+        if(cmd_bits.length>0) {
+            byteCount += data.length;
+            devnull.write(data);
+            sock.write("OK "+data.length+"\r\n");
+        }
     });
 
     sock.on("end", function() {
-        console.log("Connection closed: "+byteCount);
+        console.log("Connection closed: "+clientInfo+" "+byteCount);
     });
         
 });
